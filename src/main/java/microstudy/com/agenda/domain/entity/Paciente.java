8596adbf7855a8a7fad9e4b960d71a151827381f
Paulo Viana <paulo.viana@linkconsulting.com>
2022-09-27 23:57:03 -0300

    created tables and entities

---
+++ src/main/java/microstudy/com/agenda/domain/entity/Paciente.java
@@ -0,0 +1,26 @@
+package microstudy.com.agenda.domain.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String email;
+
+    private String cpf;
+
+}
